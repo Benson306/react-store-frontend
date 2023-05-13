560b4fe1b3bee5f3cc2190af83ea26df17301761
Benson Ndiwa <bnkimtai@gmail.com>
2023-05-13 09:32:21 +0300

    Confirm Payment

--- src/components/client/ConfirmPayment.js
+++ src/components/client/ConfirmPayment.js
@@ -10,7 +10,6 @@ const ConfirmPayment = () => {
     useEffect(()=>{
         fetch('/api/ConfirmPayment/645f19141f47848c6f199e13')
         .then(data => {
-            consol
             if(data === "Completed"){
                 setSuccess(true);
             }else if(data === "Failed"){
