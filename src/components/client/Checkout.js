bf0ace6ddf88e97b1849dc83850aae538b4106d5
Benson Ndiwa <bnkimtai@gmail.com>
2024-07-23 17:50:44 +0300

    Delivery changed

--- src/components/client/Checkout.js
+++ src/components/client/Checkout.js
@@ -158,7 +158,7 @@ const Checkout = () => {
                 <div className="p-1 capitalize text-red-900 text-xs">{props.touched.phoneNumber && props.errors.phoneNumber}</div>
             </div>
 
-            <div >
+            <div className="block w-full lg:w-1/2">
                 { !allVideos && 
                 <div>
                     <div className="font-bold mb-5">Select Delivery Location</div>
@@ -175,7 +175,7 @@ const Checkout = () => {
                 </div>
                 }
                 {
-                    allVideos && <div className="mb-8 text-lg text-zinc-700 w-3/4">* Links to our watch streams will be emailed to you via the email you have provided on <b>Account Information</b></div>
+                    allVideos && <div className="mb-8 text-md text-zinc-700 italic">* Links to our watch streams will be emailed to you via the email you have provided on <b>Account Information</b></div>
                 }
                 <div className="font-bold mb-5">Order Summary</div>
                 {
